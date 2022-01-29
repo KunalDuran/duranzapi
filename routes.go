375ca5332cc935add5793c7f6d5305ab0f31d23f
KunalDuran <kunalduran13@gmail.com>
2022-01-29 23:46:04 +0530

    updated index

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/KunalDuran/duranzapi/module/cms"
@@ -33,6 +34,52 @@ func addRouteHandlers(router *httprouter.Router) {
 
 // Plain index page content
 func index(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Welcome to Duranz API"))
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(w, sampleJSON)
 }
+
+var sampleJSON = `
+{
+    "license": "Duranz API",
+    "statusCode": 200,
+    "content": [
+        {
+            "season_id": "2021",
+            "player_id": 149,
+            "player_name": "Mohammed Shami",
+            "team_id": 12,
+            "batting": {
+                "balls_faced": 17,
+                "dot_balls_played": 7,
+                "doubles": 3,
+                "is_batted": 4,
+                "runs_scored": 13,
+                "singles": 7,
+                "average": 13,
+                "highest_score": 9,
+                "strike_rate": 76.48,
+                "not_outs": 3
+            },
+            "bowling": {
+                "balls_bowled": 316,
+                "dots_bowled": 151,
+                "extras_conceded": 8,
+                "fours_conceded": 44,
+                "maiden_over": 1,
+                "overs_bowled": "52.4",
+                "runs_conceded": 395,
+                "sixes_conceded": 14,
+                "wickets_taken": 19,
+                "economy": 7.5,
+                "average": 20.79,
+                "best_bowling": "3/21"
+            },
+            "fielding": {
+                "catches": 1
+            }
+        }
+    ]
+}`
